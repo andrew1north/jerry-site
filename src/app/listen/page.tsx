fdc30f1943aebcde8c5b7fed464e39d264a4ef00
andrew1north <116239497+andrew1north@users.noreply.github.com>
2025-05-07 20:30:52 -0700

    adding april playlist

--- src/app/listen/page.tsx
+++ src/app/listen/page.tsx
@@ -13,6 +13,17 @@ export default function Listen() {
             height="450" 
             style={{width:"100%", maxWidth:"660px", overflow:"hidden", background:"transparent"}} 
             sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
+            src="https://embed.music.apple.com/us/playlist/apr-25/pl.u-Ymb0DRRhPjEkANm"
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <iframe 
+            allow="autoplay *; encrypted-media *;" 
+            frameBorder="0" 
+            height="450" 
+            style={{width:"100%", maxWidth:"660px", overflow:"hidden", background:"transparent"}} 
+            sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
             src="https://embed.music.apple.com/us/playlist/mar-25/pl.u-Zmbl32oc0vm4N26"
           />
         </div>
